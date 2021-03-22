aba021935e71976df3652a16c8bd8b012e962894
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-03-22 16:40:49 -0500

    feat: command line flags for storing in minio s3

---
+++ internal/cmd/minios3.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"github.com/urfave/cli"
+)
+
+func MinioS3StorageCmd() []cli.Flag {
+	return []cli.Flag{
+		cli.StringFlag{
+			Name:   "s3-server",
+			Usage:  "S3 server endpoint",
+			Value:  "minio",
+			EnvVar: "MINIO_SERVICE_HOST",
+		},
+		cli.StringFlag{
+			Name:   "s3-server-port",
+			Usage:  "S3 server port",
+			EnvVar: "MINIO_SERVICE_PORT",
+		},
+		cli.StringFlag{
+			Name:     "s3-bucket",
+			Usage:    "S3 bucket where the data will be uploaded",
+			Required: true,
+		},
+		cli.StringFlag{
+			Name:     "access-key, akey",
+			Usage:    "access key for S3 server, required based on command run",
+			Required: true,
+		},
+		cli.StringFlag{
+			Name:     "secret-key, skey",
+			Usage:    "secret key for S3 server, required based on command run",
+			Required: true,
+		},
+		cli.StringFlag{
+			Name:  "input",
+			Usage: "input file that will be uploaded",
+		},
+		cli.StringFlag{
+			Name:     "upload-path,p",
+			Usage:    "full upload path inside the bucket",
+			Required: true,
+		},
+	}
+}
